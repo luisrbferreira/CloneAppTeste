5c430c3367c00db9df72e8ff60cabc66b5d6888e
Luis Ferreira <luisrbferreira@gmail.com>
2018-03-02 03:45:15 -0300

    Ajustes

--- app/src/main/java/br/com/luisferreira/cloneappteste/activities/InsertActivity.java
+++ app/src/main/java/br/com/luisferreira/cloneappteste/activities/InsertActivity.java
@@ -1,13 +1,11 @@
 package br.com.luisferreira.cloneappteste.activities;
 
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
