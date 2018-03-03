d8b0fbed9d69f17d2d8ddd2ceed8ab2471a07ec5
Luis Ferreira <luisrbferreira@gmail.com>
2018-03-03 00:39:38 -0300

    Ajuste

--- app/src/main/java/br/com/luisferreira/cloneappteste/activities/MainActivity.java
+++ app/src/main/java/br/com/luisferreira/cloneappteste/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.luisferreira.cloneappteste.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +10,18 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,13 +37,18 @@ import br.com.luisferreira.cloneappteste.adapter.RecyclerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "ANONYMOUS_FIREBASE";
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private DatabaseReference databaseReference;
+
     private RecyclerView recyclerView;
     private RecyclerAdapter recyclerAdapter;
     private FloatingActionButton fabNovoClone;
     protected ProgressBar progressBar;
 
     private List<Clone> clones = new ArrayList<>();
-    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +60,24 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Clones Cadastrados");
 
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                if (firebaseUser != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in: " + firebaseUser.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.addAuthStateListener(authStateListener);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         initViews();
-        fetchData();
     }
 
     private void initViews() {
@@ -64,6 +93,34 @@ public class MainActivity extends AppCompatActivity {
         fabNovoClone = findViewById(R.id.fabNovoClone);
 
         progressBar = findViewById(R.id.main_progress);
+    }
+
+    @Override
+    protected void onStart () {
+        super.onStart ();
+
+        firebaseAuth.addAuthStateListener (authStateListener);
+        firebaseAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "OnComplete : " + task.isSuccessful());
+
+                if (!task.isSuccessful ()) {
+                    Log.w (TAG, "Failed : ", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+
+                fetchData();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop () {
+        super.onStop ();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener (authStateListener);
+        }
     }
 
     private void fetchData() {
@@ -96,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void chamarCadastro(View view) {
+    public void callInsert(View view) {
         Intent intent = new Intent(this, InsertActivity.class);
         startActivity(intent);
     }
